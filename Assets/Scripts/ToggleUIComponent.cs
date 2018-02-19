32386df2e13e5f07fa9b91853c67b1e7fc775439
Eliot L. Carney-Seim <ecsit39@gmail.com>
2018-02-18 20:13:09 -0500

    Reorganized file structures, better moth controls.

---
+++ Assets/Scripts/ToggleUIComponent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleObject : MonoBehaviour {
+
+    public string Key;
+    public string UnityInput;
+
+    private bool isKey;
+
+    public GameObject ToToggle;
+
+	// Use this for initialization
+	void Start () {
+        var k = Input.GetAxisRaw("Inpdasut");
+        if (k == 0)
+        {
+            isKey = false;
+        }
+        else
+        {
+            isKey = true;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (isKey)
+        {
+            if (Input.GetKeyDown(Key))
+            {
+                ToToggle.SetActive(!ToToggle.activeSelf);
+            }
+        }	
+	}
+}
