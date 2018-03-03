da3a11edb11bf95ed7c7d91a96b04eb54c49b3c4
Eliot L. Carney-Seim <ecsit39@gmail.com>
2018-03-03 16:43:29 -0500

    Implementing UI in actual game, halfway done.

---
+++ Assets/Scripts/ToggleComponentWithButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleComponentWithButton : MonoBehaviour {
+
+    private Component[] components;
+
+    public string ToggleButton;
+
+	// Use this for initialization
+	void Start () {
+        components = gameObject.GetComponents<Component>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+
+
+}
